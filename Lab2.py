f27d3ce165be415cb4f94bcd01e215b3b1dead34
nicodani <danescobar@udec.cl>
2016-04-17 17:06:46 +0000

    Llevando_imagenes_a_escala_de_grises

--- Lab2.py
+++ Lab2.py
@@ -1,7 +1,15 @@
 #! /usr/bin/python
-from SimpleCV import Camera, Display, Image
-import time
-c=Camera()
-img=c.getImage()
-img.save("PapelColor.jpg")
-
+from SimpleCV import Camera, Display, Image ## Cargamos los paquetes de SimpleCV en Python
+img=Image("PapelBlanco.jpg") ##Cargamos archivo de imagen
+imggs=img.grayscale() ##Llevamos la imagen a escala de grises
+imggs.save("PapelBlanco(gs).jpg") ## Guardamos archivo de imagen en escala de grises
+## Repetimos lo anterior para todas las imagenes tomadas
+img2=Image("PapelCuadriculado.jpg")
+img2gs=img2.grayscale()
+img2gs.save("PapelCuadriculado(gs).jpg")
+img3=Image("PapelCuadriculadocolor.jpg") 
+img3gs=img3.grayscale()
+img3gs.save("PapelCuadriculadocolor(gs).jpg")
+img4=Image("PapelColor.jpg")
+img4gs=img4.grayscale()
+img4gs.save("PapelColor(gs).jpg")
